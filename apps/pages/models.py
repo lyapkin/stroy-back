a29b840b17ee76f46d28a0f9b3020c9e80d3e33d
lyapkin <lyapkin992@gmail.com>
2025-06-11 17:39:01 +0500

    change field length in page model

--- apps/pages/models.py
+++ apps/pages/models.py
@@ -7,7 +7,7 @@ class Page(models.Model):
     name = models.CharField("страница", max_length=255, unique=True)
     slug = models.SlugField(max_length=64, unique=True)
 
-    title = models.CharField("заголовок страницы", max_length=72)
+    title = models.CharField("заголовок страницы", max_length=88)
 
     def __str__(self):
         return self.name
