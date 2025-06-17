0e5089535183137ec2a5f806c871d4cb2700ff53
lyapkin <lyapkin992@gmail.com>
2025-06-17 13:37:52 +0500

    refactor added table to ck

--- core/settings.py
+++ core/settings.py
@@ -178,14 +178,6 @@ REST_FRAMEWORK = {
 
 
 # ckeditor
-customColorPalette = [
-    {"color": "hsl(4, 90%, 58%)", "label": "Red"},
-    {"color": "hsl(340, 82%, 52%)", "label": "Pink"},
-    {"color": "hsl(291, 64%, 42%)", "label": "Purple"},
-    {"color": "hsl(262, 52%, 47%)", "label": "Deep Purple"},
-    {"color": "hsl(231, 48%, 48%)", "label": "Indigo"},
-    {"color": "hsl(207, 90%, 54%)", "label": "Blue"},
-]
 
 CKEDITOR_5_CUSTOM_CSS = "css/ckeditor5/style.css"  # optional
 CKEDITOR_5_FILE_STORAGE = "shared.utils.CustomStorage"  # optional
@@ -205,7 +197,12 @@ CKEDITOR_5_CONFIGS = {
             "sourceEditing",
             "bulletedList",
             "numberedList",
+            "|",
+            "insertTable",
         ],
+        "table": {
+            "contentToolbar": ["tableColumn", "tableRow"],
+        },
         "heading": {
             "options": [
                 {"model": "paragraph", "title": "Параграф", "class": "ck-heading_paragraph"},
@@ -245,11 +242,6 @@ CKEDITOR_5_CONFIGS = {
                 "alignCenter",
             ],
         },
-        # "table": {
-        #     "contentToolbar": ["tableColumn", "tableRow", "mergeTableCells", "tableProperties", "tableCellProperties"],
-        #     "tableProperties": {"borderColors": customColorPalette, "backgroundColors": customColorPalette},
-        #     "tableCellProperties": {"borderColors": customColorPalette, "backgroundColors": customColorPalette},
-        # },
         "heading": {
             "options": [
                 {"model": "paragraph", "title": "Параграф", "class": "ck-heading_paragraph"},
