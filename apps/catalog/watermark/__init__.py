ef70d3d66dfab5295693200b906c4885ef464f56
lyapkin <lyapkin992@gmail.com>
2025-06-30 02:14:31 +0500

    .

--- apps/catalog/watermark/__init__.py
+++ apps/catalog/watermark/__init__.py
@@ -22,6 +22,8 @@ def watermark_img(path):
     x = (original.width - watermark.width) // 2
     y = (original.height - watermark.height) // 2
     original.paste(watermark, (x, y), watermark)
-    if original.format == "JPEG":
+    try:
+        original.save(path)
+    except OSError as e:
         original = original.convert("RGB")
-    original.save(path)
+        original.save(path)
