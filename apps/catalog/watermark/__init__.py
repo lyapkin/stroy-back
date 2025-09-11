e3eeee209d0fae2cd96219444d183daf7010a20f
lyapkin <lyapkin992@gmail.com>
2025-09-11 15:08:25 +0500

    added watermark for animated imgs

--- apps/catalog/watermark/__init__.py
+++ apps/catalog/watermark/__init__.py
@@ -1,11 +1,11 @@
 import os
-from PIL import Image
+from PIL import Image, ImageSequence
 from django.conf import settings
 
 
 def watermark_img(path):
     watermark_logo_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logo.png")
-    original = Image.open(path).convert("RGBA")
+    original = Image.open(path)
     watermark = Image.open(watermark_logo_path).convert("RGBA")
 
     # Resize watermark if needed (example: resize to 20% of original image width)
@@ -21,9 +21,36 @@ def watermark_img(path):
     # Paste watermark
     x = (original.width - watermark.width) // 2
     y = (original.height - watermark.height) // 2
-    original.paste(watermark, (x, y), watermark)
+    frames = ImageSequence.all_frames(original, lambda i: place_watermark(i, watermark, x, y))
+    if len(frames) > 1:
+        watermark_animated(original, frames, path)
+    else:
+        watermark_static(frames[0], path)
+
+
+def watermark_animated(img: Image, frames: list[Image.Image], path):
+
+    frames[0].save(
+        path,
+        save_all=True,
+        append_images=frames[1:],
+        duration=img.info.get("duration", 100),
+        timestamp=img.info.get("timestamp", 4100),
+        loop=0,
+        minimize_size=True,
+    )
+
+
+def watermark_static(img, path):
+
     try:
-        original.save(path)
+        img.save(path)
     except OSError as e:
-        original = original.convert("RGB")
-        original.save(path)
+        img = img.convert("RGB")
+        img.save(path)
+
+
+def place_watermark(frame, watermark, x, y):
+    frame = frame.convert("RGBA")
+    frame.paste(watermark, (x, y), watermark)
+    return frame
